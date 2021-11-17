507e07b327ac68c54425ca46fb5c2226ed9ff332
Dorian GRIMAUD <DGRIMAUD>
2021-11-17 14:19:47 +0100

    iniatialisation

---
+++ src/app/details/details.page.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute} from '@angular/router';
+import { NavigationExtras } from '@angular/router';
+@Component({
+  selector: 'app-details',
+  templateUrl: './details.page.html',
+  styleUrls: ['./details.page.scss'],
+})
+export class DetailsPage implements OnInit {
+
+  item="";
+
+
+  constructor(private router: Router, private activeRoute : ActivatedRoute) { 
+    this.activeRoute.queryParams.subscribe(params => {
+      if(this.router.getCurrentNavigation().extras.state){
+        this.item = 
+        this.router.getCurrentNavigation().extras.state.param1;
+      }
+      
+    });
+  }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Router} from '@angular/router';
+import { NavigationExtras } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage{
 
-  constructor() {}
+
+  ListeArret:any;
+  constructor(private http: HttpClient,private router:Router) {
+    this.http.get("http://localhost:8001/api/hackathons")
+    .subscribe(results => {
+      this.ListeArret=results;
+    });
+  }
+  
+  MonClick(item){
+    let navigationExtras: NavigationExtras = {
+      state : {
+      param1: item
+      }
+      };
+    this.router.navigate(['/details'], navigationExtras);
+  }
+  
+  
+  ngOnInit() {
+  }
 
 }
